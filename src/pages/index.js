51b8d6dc27eab6b06a302b0e7c0a9dd6cf6e7a8c
Pavel Mineev <pavel@mineev.me>
2021-05-29 13:15:29 +0300

    update markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,31 +2,33 @@ import Link from "next/link";
 import { useRouter } from "lib/router";
 
 export default function Index() {
-	const { query } = useRouter();
+  const { query } = useRouter();
 
-	return (
-		<>
-			<h1>Next.js Router – Tips &amp; Tricks</h1>
-			{query["redirected-from"] === "/ssr" && (
-				<span className="alert">You have been redirected from /ssr</span>
-			)}
-			<ul>
-				<li>
-					<Link href="/params">
-						<a>Router Client Functions</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/ssr">
-						<a>Server-Side Props Redirect</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/not-found">
-						<a>Server-Side Not Found</a>
-					</Link>
-				</li>
-			</ul>
-		</>
-	);
+  return (
+    <>
+      <p>
+        This is the example for an <a href="">article about Next.js router</a>
+      </p>
+      {query["redirected-from"] === "/ssr" && (
+        <span className="alert">You have been redirected from /ssr</span>
+      )}
+      <ul>
+        <li>
+          <Link href="/params">
+            <a>Router Client Functions</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/ssr">
+            <a>Server-Side Props Redirect</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/not-found">
+            <a>Server-Side Not Found</a>
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 }
